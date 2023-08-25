517f8f76146eadea73cfd5bcf4f7d45eae3a6127
Luis Guaiquirian <luisdavidguaiquirian@gmail.com>
2023-08-24 21:42:09 -0400

    Adding styles at the end

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,11 @@
 "use strict";
+const btn = document.querySelector(".main-hero-form-btn");
+const input = document.querySelector(".main-hero-form-input");
+const errorMessage = document.querySelector(".main-hero-form-error");
+
+btn.addEventListener("click", function () {
+   if (input.checkValidity() == false) {
+      errorMessage.style.visibility = "visible";
+      input.style.border = "1px solid red";
+   }
+});
